49b06c953beb2ad6cbf3e8efb00463b66a6c1bb5
Hamza Sharif <160274743+Imhamzasharif@users.noreply.github.com>
2024-12-20 08:43:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+// 1. Declare 3 variables in one statement.
+
+var name = "hamza", rollNumber = 327366, batch = true;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// these are legal variabls
+
+let myName = ("hamza");
+console.log(myName);
+
+let $classroom = ("SMIT");
+console.log($classroom);
+
+let _assignments = ("chapter04");
+console.log(_assignments);
+
+let pro123 = ("againclick");
+console.log(pro123);
+
+let money = (30000);
+console.log(money);
+
+// these are illegal variables
+
+// 12user;
+// my-Worker;
+// my project;
+// #usinghashtagsign;
+// var;
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+document.write("Rules for naming JS variables\n\n");
+
+document.write("Variable names can only contain name, dollarsign, underscore_ and numbers/digits in the end\n\n");
+document.write("Variables must begin with a letters, dollarsign or underscore.\n\n");
+document.write("Variable names are case camleCase\n\n");
+document.write("Variable names should not be JS keywords, it means js k jo variables hain wo jskeywords per na ho jesy let, var, const etc.\n\n");
